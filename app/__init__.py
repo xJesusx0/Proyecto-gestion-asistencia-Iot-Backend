4562e80e19dafc1aed44aae1ab51c2524ce41a4e
jesus <jesuspl0411@gmail.com>
2024-07-18 14:57:33 -0500

    se agrego un error handler en caso de que se intente acceder a una ruta no encontrada

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,33 @@
-from flask import Flask
+from flask import Flask,jsonify
 from flask_cors import CORS
 from flask_session import Session
-import os
 from flask_mysqldb import MySQL
+
 from app.config import Config
 
 from app.routes.auth import auth_bp
 from app.routes.administrators import admin_bp
 from app.routes.students import students_bp
+from app.routes.esp32 import esp32_bp
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
-app.secret_key = 'secret'
 
 Session(app)
 CORS(app, supports_credentials=True)
 mysql = MySQL(app)
 
-@app.route('/')
-def index():
-    return 'hola mundo'
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({'error':404,'response':'Ruta no encontrada'}), 404
 
 auth_bp.mysql = mysql
 admin_bp.mysql = mysql
 students_bp.mysql = mysql
+esp32_bp.mysql = mysql
+
 app.register_blueprint(auth_bp)
 app.register_blueprint(admin_bp)
 app.register_blueprint(students_bp)
+app.register_blueprint(esp32_bp)
