3c380ea078ed118d7873d72811e98775d19c35aa
jesus <jesuspl0411@gmail.com>
2024-07-20 17:09:00 -0500

    consulta para obtener las asistencias

--- Database/attendances.py
+++ Database/attendances.py
@@ -11,5 +11,13 @@ def attendance_exists(mysql,cursor,student_id,group_id,module_id,period,date):
     return None
 
 @handle_database_operations
+def get_attendances_by_id(mysql,cursor,student_id,group_id,module_id,period):
+    cursor.execute('SELECT * from asistencias WHERE id_estudiante = %s AND id_grupo = %s AND id_modulo = %s AND periodo = %s',(student_id,group_id,module_id,period))
+    response = cursor.fetchall()
+    if response:
+        return response
+        
+    return None
+@handle_database_operations
 def insert_attendance(mysql,cursor,student_id,group_id,module_id,period,date,time):
     return 'ok'
