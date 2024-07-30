639c536e2e61ee715adab987ea3d2959ca4e71e8
jesus <jesuspl0411@gmail.com>
2024-07-30 16:07:13 -0500

    funcion que agrega las asistencias a los estudiantes

--- Database/attendances.py
+++ Database/attendances.py
@@ -21,3 +21,14 @@ def get_attendances_by_id(mysql,cursor,student_id,group_id,module_id,period):
 @handle_database_operations
 def insert_attendance(mysql,cursor,student_id,group_id,module_id,period,date,time):
     cursor.execute('INSERT INTO asistencias (id_estudiante, id_grupo, id_modulo, periodo, fecha, hora_llegada) VALUES (%s, %s, %s, %s, %s, %s)',(student_id,group_id,module_id,period,date,time))
+
+@handle_database_operations
+def insert_group_attendance(mysql,cursor,students_list,group_id,module_id,period,date,time):
+    
+    query = 'INSERT INTO asistencias (id_estudiante,id_grupo,id_modulo,periodo,fecha,hora_llegada) VALUES (%s,%s,%s,%s,%s,%s)'
+    data = [(student_id,group_id,module_id,period,date,time) for student_id in students_list]
+
+    try:
+        cursor.executemany(query,data)
+    except Exception as e:
+        return e
