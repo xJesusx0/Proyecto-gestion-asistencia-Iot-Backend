d09b9d2f99f210cc660dc676698d12c2dde5a5db
jesus <jesuspl0411@gmail.com>
2024-08-01 12:30:54 -0500

    consulta para obtener los estudiantes que no tienen faltas

--- Database/attendances.py
+++ Database/attendances.py
@@ -32,3 +32,39 @@ def insert_group_attendance(mysql,cursor,students_list,group_id,module_id,period
         cursor.executemany(query,data)
     except Exception as e:
         return e
+
+@handle_database_operations
+def get_attendances_by_day(mysql,cursor,group_id,module_id,period,date):
+    cursor.execute('select asistencias.*,usuarios.nombres,usuarios.apellidos from asistencias join usuarios on usuarios.id_usuario = asistencias.id_estudiante where id_grupo = %s and id_modulo = %s and periodo = %s and fecha = %s order by asistencias.hora_llegada',(group_id,module_id,period,date))
+    res = cursor.fetchall()
+    if res:
+        return res
+    
+    return None
+
+@handle_database_operations
+def get_students_without_attendance_by_group(mysql,cursor,group_id,module_id,period,date):
+    
+    query = """
+        SELECT matricula.id_estudiante, usuarios.nombres,usuarios.apellidos FROM matricula 
+        JOIN usuarios ON matricula.id_estudiante = usuarios.id_usuario 
+        LEFT JOIN asistencias  ON matricula.id_estudiante = asistencias.id_estudiante
+        AND asistencias.id_grupo = matricula.id_grupo    
+        AND asistencias.id_modulo = matricula.id_modulo    
+        AND asistencias.periodo = matricula.periodo    
+        AND asistencias.fecha = %s 
+        WHERE matricula.id_grupo = %s   
+        AND matricula.id_modulo = %s 
+        AND matricula.periodo = %s
+        AND asistencias.id_estudiante IS NULL;            
+        """
+
+    values = (date,group_id,module_id,period)
+
+
+    cursor.execute(query,values)
+    res = cursor.fetchall()
+    if res:
+        return res
+    
+    return None
