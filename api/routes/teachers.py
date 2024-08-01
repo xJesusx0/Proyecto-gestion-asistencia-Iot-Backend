acf9cdd603e790e1f2b445c16ddce7e208d89f31
jesus <jesuspl0411@gmail.com>
2024-08-01 12:31:25 -0500

    ruta para obtener los estudiantes que no tienen faltas y validaciones a la hora de poner asistencia

--- api/routes/teachers.py
+++ api/routes/teachers.py
@@ -7,8 +7,8 @@ from api.routes import get_now,get_day
 from Database.teachers import *
 from Database.groups import get_teacher_group,get_group_details
 from Database.students import get_students_by_group
-from Database.attendances import insert_group_attendance
-from Database.fails import get_absent_students_by_date,insert_fails
+from Database.attendances import insert_group_attendance,get_attendances_by_day,get_students_without_attendance_by_group
+from Database.fails import get_absent_students_by_date,insert_fails,get_fails_by_id_and_group
 from Database import encode_time
 
 teachers_bp = Blueprint('teachers',__name__,url_prefix='/teachers')
@@ -46,6 +46,29 @@ def get_teacher_group_details():
     
     group = encode_time(group)
     return jsonify(group),200
+
+
+@teachers_bp.route('/get-students-without-attendance-by-group')
+@jwt_required()
+@valid_login
+@valid_role('get-students-without-attendance-by-group')
+def get_students_wo_attendances_by_group():
+    group_id = request.args.get('group_id')
+    module_id = request.args.get('module_id')
+    period = request.args.get('period')
+
+    if not group_id or not module_id or not period:
+        return jsonify({'error':'Hacen falta argumentos'}),400
+
+    now = get_now()
+    current_date = now.strftime("%Y-%m-%d")
+
+    students = get_students_without_attendance_by_group(group_id,module_id,period,current_date)
+    
+    if not students:
+        return jsonify({'response':'No hay estudiantes asignados a este grupo'})
+    
+    return jsonify(students)
 
 @teachers_bp.route('/get-students-by-group')
 @jwt_required()
@@ -104,11 +127,18 @@ def set_group_attendance():
         return jsonify({'error':'La clase ya finalizo'}),404
     
     day_info = [current_date,current_time,day]
+
+    for student in students:
+        fail_exist = get_fails_by_id_and_group(student,group_id,module_id,period)
+        print(fail_exist)
+
+        if fail_exist:
+            return jsonify({'error':f'El estudiante {student} tiene una falla registrada hoy'}),409
+
     res = insert_group_attendance(students,group_id,module_id,period,current_date,current_time)
-    
     if res:
         return jsonify({'error':str(res)})
-        
+    print(res) 
     return jsonify({'data':request_body,'day':day_info})
 
 
@@ -132,11 +162,9 @@ def set_group_fails():
         return jsonify({'error':'Hacen falta campos'}),400
 
     now = get_now()
-    current_time = now.strftime("%H:%M:%S")
     current_date = now.strftime("%Y-%m-%d")
-    day = get_day(now.strftime("%A"))  
 
-    students = absent_students = get_absent_students_by_date(current_date,group_id,module_id,period) 
+    students = get_absent_students_by_date(current_date,group_id,module_id,period) 
     
     if not students:
         return jsonify({'error':'Ha ocurrido un error al obtener los estudiantes'}),404
@@ -147,3 +175,29 @@ def set_group_fails():
     if res:
         return jsonify({'error':f'Ha ocurrido un error al insertar las inasistencias:\n{res}'}),500
     return jsonify({'response':'ok'})
+
+@teachers_bp.route('/get-day-attendances')
+@jwt_required()
+@valid_login
+@valid_role('get-day-attendances')
+def get_day_attendances():
+
+    
+    group_id = request.args.get('group_id')
+    module_id = request.args.get('module_id')
+    period = request.args.get('period')
+    
+    if not group_id or not module_id or not period:
+        return jsonify({'error':'Hacen falta campos'}),400
+
+    now = get_now()
+    current_date = now.strftime("%Y-%m-%d")
+
+    attendances = get_attendances_by_day(group_id,module_id,period,current_date)
+    if not attendances:
+        return jsonify({'error':'No hay asistencias el dia de hoy'}),404
+
+    attendances_json = encode_time(attendances)
+    
+    return jsonify(attendances_json),200
+ 
